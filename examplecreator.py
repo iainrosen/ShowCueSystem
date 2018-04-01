074c49fa0dff6a1494eb12f355d3f6bcf45ec921
Iain Rosen <iain@tridentcsd.com>
2018-04-01 13:50:03 -0700

    Created Script for User Examples

---
+++ examplecreator.py
@@ -0,0 +1,29 @@
+import scs
+import random
+import time
+print "Writing Example Show File..."
+scs.newshow("example", "Example Show", "Iain Rosen")
+r = 0
+while True:
+    rndgo = random.randint(0,2)
+    if rndgo == 0:
+        desc = "LX GO"
+    elif rndgo == 1:
+        desc = "SFX GO"
+    elif rndgo == 2:
+        desc = "LX FADE"
+    else:
+        print "Error"
+    if r == 0:
+        c = 1
+    else:
+        c = random.randint(1, 999)
+    c = str(c)
+    show = "example"
+    scs.crec(c, show, desc)
+    r = r + 1
+    if r == 500:
+        print "Cues Created: " + str(r)
+        time.sleep(5)
+        break
+    
